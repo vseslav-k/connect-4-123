bfd1536a3939e52821bbfa88fa04fe02ac54dc63
vseslav-k <vseslavvak@mail.ru>
2026-02-12 22:42:47 -0800

    added dynamic difficulty scaling

--- classes/Connect4.cpp
+++ classes/Connect4.cpp
@@ -1,7 +1,7 @@
 #include "Connect4.h"
 #include "C:\Libraries\imgui\Timer\Timer.h"
 Timer timer = Timer();
-int Connect4::assessWinPattern(const Board& board, Color color, int patternIdx){
+int Connect4::assessWinPattern(const Board& board, const Color color, const int patternIdx) const{
     if((WINNING_PATTERNS[patternIdx] & board.pieces[!color]) != 0)
         return 0;
 
@@ -12,7 +12,7 @@ int Connect4::assessWinPattern(const Board& board, Color color, int patternIdx){
 }
 
 
-int Connect4::evalBoardState(const Board& board, Color color){
+int Connect4::evalBoardState(const Board& board, const Color color) const{
     int score = 0;
     int oppScore = 0;
 
@@ -23,6 +23,19 @@ int Connect4::evalBoardState(const Board& board, Color color){
 
     return score - (oppScore/1.2);
 
+}
+
+
+std::array<int, 42> Connect4::makeHeatMap(const uint64_t *arr, const int len){
+    std::array<int, 42> res{};
+
+    for(int i = 0; i < len; ++i){
+        for(int j = 0; j < 42; ++j)
+            res[j] += getBit(arr[i], j);
+        
+    }
+
+    return res;
 }
 
 std::pair<int, int> Connect4::cordsBoardToGrid(int boardIdx){
@@ -58,9 +71,10 @@ Connect4::Connect4(int aiPlayer){
     _board.pieces[RED] = 0;
     _board.pieces[YELLOW] = 0;
 
-    std::string boards = ptrToStr(WINNING_PATTERNS.data(), WINNING_PATTERNS.size(), formatBoard, "\n");
+    std::array<int, 42> heatmap = makeHeatMap(WINNING_PATTERNS.data(), WINNING_PATTERNS.size());
+    std::string heatmapStr = ptrToStr(heatmap.data(), heatmap.size(), numToStr, " ");
 
-    //log(Debug, boards);
+    log(Debug, heatmapStr);
     
 }
 Connect4::~Connect4(){
@@ -91,8 +105,30 @@ bool Connect4::checkForDraw(){
 }
 
 bool Connect4::boardIsFull(){
-    return ((_board.pieces[RED] | _board.pieces[YELLOW]) == FULL_BOARD);
+    return ((_board.pieces[RED] | _board.pieces[YELLOW]) == UTIL_PATTERNS[FULL]);
 }
+
+
+bool Connect4::comboWon(const uint64_t piecies) const{
+
+
+    return 
+    
+    // Horizontal (stride 1)
+    (piecies & (piecies << 1) & (piecies << 2) & (piecies << 3) & UTIL_PATTERNS[HORIZONTAL_START]) ||
+
+    // Vertical (stride 7)
+    (piecies & (piecies << 7) & (piecies << 14) & (piecies << 21) & UTIL_PATTERNS[VERTICAL_START]) ||
+
+
+    // Diagonal LR '\' (stride 8)
+    (piecies & (piecies << 8) & (piecies << 16) & (piecies << 24) & UTIL_PATTERNS[DIAGONAL_LR_START]) ||
+    
+    // Diagonal RL '/' (stride 6)
+    (piecies & (piecies << 6) & (piecies << 12) & (piecies << 18) & UTIL_PATTERNS[DIAGONAL_RL_START]);
+    
+}
+
 
 void Connect4::setUpBoard() {
     setNumberOfPlayers(2);
@@ -197,25 +233,27 @@ void Connect4::stopGame(){
     _board.pieces[YELLOW] = 0;
 }
 
+
+
 void Connect4::updateAI(){
     if(aiPlayer != getCurrentPlayer()->playerNumber()) return;
 
     int bestMoveIdx = -1;
 
-    int bestScore = -999999999;
-    int res = -999999;
+    int bestScore = -MATE*100;
+    int res = -MATE/10;
 
-    int d = 7;
+    int d = DEPTH_AT_TURN[(this->_turns.size()-1)/2];
 
-    d += std::popcount(_board.pieces[RED] | _board.pieces[YELLOW])/(7);
 
-    log(Debug, "d= "+numToStr(d));
+    log(Debug, "Turn: " + numToStr(this->_turns.size()));
+    log(Debug, "Depth: " + numToStr(d));
     timer.setPt("AI Thinking Start");
     for(int i = 0; i < 42; ++i){
         if(!moveIsLegal((_board.pieces[RED] | _board.pieces[YELLOW]), i)) continue;
 
         setBitInPlace(_board.pieces[aiPlayer], i, true);
-        res = -negamax(static_cast<Color>(!aiPlayer), -99999999999, 99999999999, d);
+        res = -negamax(static_cast<Color>(!aiPlayer), -MATE, MATE, d);
         setBitInPlace(_board.pieces[aiPlayer], i, false);
 
         if(res > bestScore){
@@ -225,7 +263,7 @@ void Connect4::updateAI(){
 
     }
     timer.setPt("AI Thinking End");
-    log(Info, "AI thought for: "+fltToStr(timer.milliPassed("AI Thinking Start", "AI Thinking End")));
+    log(Info, "Thinking Time: "+fltToStr(timer.milliPassed("AI Thinking Start", "AI Thinking End")));
 
     if(bestMoveIdx == -1) {
         log(Error, "No legal moves available");
@@ -239,16 +277,11 @@ void Connect4::updateAI(){
 }
 
 
-bool Connect4::moveIsLegal(uint64_t board, int i){  
-    if(getBit(board, i) == 1) return false;
+bool Connect4::moveIsLegal(const uint64_t board, const int i) const{
 
-    if( i >= 35) return true;
+    return (getBit(board, i) == 0 && ( (i>=35) || ((getBit(board, i+7) == 1))));
 
-    if(getBit(board, i+7) == 1) return true;
-
-    return false;
 }
-
 
 int Connect4::legalMoveInCol(int col){
     int moveIdx = 41 - (7-col);
@@ -269,39 +302,43 @@ int Connect4::legalMoveInCol(int col){
     */
 }
 
-int Connect4::negamax(Color player, int a, int b, int d)
-{   int score = evalBoardState(_board, player);
+int Connect4::negamax(const Color player, int a, const int b, const int d){   
 
-    Player* winner = checkForWinner();
-
-
-    if(winner){
-        if(winner->playerNumber() == player){ 
-            return MATE/(d+1);
-        }
-        if(winner->playerNumber() == !player){ 
-            return -MATE*(d+1);
-        }
+    switch(comboWon(_board.pieces[player]) *1 + comboWon(_board.pieces[!player]) *2 + boardIsFull()*3){
+        case 1: return MATE/(d+1);
+        case 2: return -MATE*(d+1);
+        case 3: return 0;
+        case 4: return MATE/(d+1);
+        case 5: return -MATE*(d+1);
     }
 
-
-
+    /* equivalent to
+    if(comboWon(_board.pieces[player])){ 
+        return MATE/(d+1);
+    }
+    if(comboWon(_board.pieces[!player])){ 
+        return -MATE*(d+1);
+    }
     if(boardIsFull()){
         return 0;
     }
+    */
+
+
     if(d <= 0){
-        return score;
+        return evalBoardState(_board, player);
     }
 
     int bestScore = -MATE;
     int res = -MATE/10;
 
     for(int i = 0; i < 42; ++i){
-        if(!moveIsLegal(_board.pieces[RED] | _board.pieces[YELLOW], i)) continue;
+        int bestMove = SORTED_CELL_VALUES[i];
 
-        setBitInPlace(_board.pieces[player], i, true);
+        if(!moveIsLegal(_board.pieces[RED] | _board.pieces[YELLOW], bestMove)) continue;
+        setBitInPlace(_board.pieces[player], bestMove, true);
         res = -negamax(static_cast<Color>(!player), -b, -a, d-1);
-        setBitInPlace(_board.pieces[player], i, false);
+        setBitInPlace(_board.pieces[player], bestMove, false);
 
         bestScore = std::max(bestScore, res);
         a = std::max(a, res);
--- classes/Connect4.h
+++ classes/Connect4.h
@@ -7,6 +7,7 @@
 #include "C:\Libraries\imgui\logger\logger.h"
 
 
+
 inline constexpr std::array<uint64_t, 69> calcWinningPatterns(){ 
         std::array<uint64_t, 69> winningPatterns{};
         //horizontal _
@@ -42,10 +43,36 @@ inline constexpr std::array<uint64_t, 69> calcWinningPatterns(){
         
 }
 
+
+ inline constexpr std::array<uint64_t, 19> makeUtilPatterns(){
+    std::array<uint64_t, 19> utilPatterns{};
+
+    utilPatterns[0] = 0ULL;
+    utilPatterns[1] = 0b1111111111111111111111111111111111111111110000000000000000000000;
+    utilPatterns[2] = 0b1000000100000010000001000000100000010000000000000000000000000000;
+
+    for(int i = 3; i < 9; ++i)
+        utilPatterns[i] = utilPatterns[i-1] >> 1;
+
+    
+    utilPatterns[9] = 0b1111111000000000000000000000000000000000000000000000000000000000;
+
+    for(int i = 10; i < 15; ++i)
+        utilPatterns[i] = utilPatterns[i-1] >> 7;
+
+
+    utilPatterns[15] = utilPatterns[2] | utilPatterns[3] | utilPatterns[4] | utilPatterns[5];
+    utilPatterns[16] = utilPatterns[9] | utilPatterns[10] | utilPatterns[11];
+    utilPatterns[17] = utilPatterns[15] & utilPatterns[16];
+    utilPatterns[18] = (utilPatterns[5] | utilPatterns[6] | utilPatterns[7] | utilPatterns[8]) & utilPatterns[16];
+
+    return utilPatterns;
+ }
+
 inline uint64_t setBit(uint64_t x, unsigned pos, bool value) {
     if (pos >= 64) {
         log(Error, "Bit position out of range");
-        return 0;
+        return x;
     }
 
     // 0 = MSB, 63 = LSB
@@ -88,14 +115,28 @@ class Connect4 final: public Game{
 
 public:
 
-    enum Color{
+    enum Color: bool{
         RED = 0,
         YELLOW = 1
     };
 
+    enum UtilPatternIdx{
+        EMPT = 0,
+        FULL = 1,
+
+        COL1 = 2,COL2 = 3,COL3 = 4,COL4 = 5,COL5 = 6,COL6 = 7,COL7 = 8,
+
+        ROW1 = 9, ROW2 = 10, ROW3 = 11, ROW4 = 12, ROW5 = 13, ROW6 = 14,
+        HORIZONTAL_START = 15, VERTICAL_START = 16, DIAGONAL_LR_START = 17, DIAGONAL_RL_START = 18 
+
+    };
+
+
     struct Board{
         std::array<uint64_t, 2> pieces;
     };
+
+
 
 
 
@@ -104,6 +145,8 @@ public:
 
     void        setUpBoard() override;
     Player*     checkForWinner() override;
+    bool        comboWon(const uint64_t piecies) const;
+
     bool        checkForDraw() override;
     std::string initialStateString() override;
     std::string stateString() override ;
@@ -119,10 +162,17 @@ public:
     Grid* getGrid() override final { return _grid; }
 private:
 
-    Board _board;
     static constexpr std::array<uint64_t, 69> WINNING_PATTERNS = calcWinningPatterns();
-    static constexpr uint64_t FULL_BOARD = 0b1111111111111111111111111111111111111111110000000000000000000000;
-    static constexpr uint64_t MID_COLUMN = 0b0001000000100000010000001000000100000010000000000000000000000000;
+    static constexpr std::array<uint64_t, 19> UTIL_PATTERNS = makeUtilPatterns();
+
+    static constexpr std::array<uint8_t, 42> SORTED_CELL_VALUES = {24, 17, 23, 25, 16, 18, 31, 10, 22, 26, 30, 32, 9, 11, 15, 19, 38, 3, 29, 33, 8, 12, 21, 27, 37, 39, 2, 4, 14, 20, 28, 34, 36, 40, 1, 5, 7, 13, 35, 41, 0, 6};
+
+    //                                         idx pairs:    1-2 3-4 5-6 7-8 9-10 11-12 13-14 15-16 17-18 19-20 21-22 23-24 25-26 27-28 29-30 31-32 33-34 35-36 37-38 39-40 41-42
+    //                                     max calc left: 41  39  37  35  33  31    29    27    25    23    21    19    17    15    13    11    9     7     5     3     1
+    static constexpr std::array<uint8_t, 42> DEPTH_AT_TURN = {9,  9,  8,  9,  10,  12,   13,   14,   17,   30,   20,   20,   20,   20,   20,   10,   10,   10,   10,   10,  10};
+
+
+
     static constexpr int32_t MATE = 9999;
 
     bool isLowest(const BitHolder &holder) const;
@@ -131,22 +181,35 @@ private:
 
     Bit* PieceForPlayer(int player);
 
-    int assessWinPattern(const Board& board, Color color, int patternIdx);
-    int evalBoardState(const Board& board, Color color);
+    int assessWinPattern(const Board& board, const Color color, const int patternIdx) const;
+    int evalBoardState(const Board& board, const Color color) const;
 
     std::pair<int, int> cordsBoardToGrid(int boardIdx);
     int cordsGridToBoard(std::pair<int, int> gridCords);
 
-    int negamax(Color player, int a = -99999999999, int b = 99999999999, int d = 8);
+
+    int negamax(const Color player, int a = -MATE, const int b = MATE, const int d = 8);
+    
     int legalMoveInCol(int col);
-    bool moveIsLegal(uint64_t board, int i);
+    bool moveIsLegal(const uint64_t board, const int i) const;
     bool boardIsFull();
 
+    static std::array<int, 42> makeHeatMap(const uint64_t *arr, const int len);
 
     int aiPlayer;
 
     // Board representation
     Grid*       _grid;
+    Board _board;
+
+     /*
+    00 01 02 03 04 05 06 
+    07 08 09 10 11 12 13 
+    14 15 16 17 18 19 20 
+    21 22 23 24 25 26 27 
+    28 29 30 31 32 33 34 
+    35 36 37 38 39 40 41
+    */
 
 };
 
